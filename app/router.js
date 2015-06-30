b4f7597932c2149390700300bf54b34a08913577
Megan Dunbar <meganleedunbar@gmail.com>
2015-06-30 15:32:24 -0400

    CRUD functional

--- app/router.js
+++ app/router.js
@@ -6,6 +6,11 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('pins', function() {
+    this.route('create');
+    this.route('loading');
+    this.route('show', {path: '/:pin_id'})
+  });
 });
 
 export default Router;
