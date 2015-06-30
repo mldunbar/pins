f83c45c8aa9533f3998b1527691bcfcfeca3615f
Megan Dunbar <meganleedunbar@gmail.com>
2015-06-30 13:20:49 -0400

    added API key and updated ember and ember-data

--- config/environment.js
+++ config/environment.js
@@ -14,9 +14,20 @@ module.exports = function(environment) {
     },
 
     APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+      applicationId: 'oh6KgKEvEcdIn5FUY0na5FL3H6rs1hTkZcMF9Wsn',
+      restApiId: 'l3yVwZ0pmXEnyjD1RcCSUcuZgjLWK07wIw1FEct2'
     }
+
+    contentSecurityPolicy: {
+      'default-src': "'none'",
+      'script-src': "'self'",
+      'font-src': "'self'",
+      'connect-src': "'self' api.parse.com",
+      'img-src': "'self'",
+      'style-src': "'self'",
+      'media-src': "'self'"
+    }
+
   };
 
   if (environment === 'development') {
